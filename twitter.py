0000142f7035c4f4115308c22ea14fa4bf20a08b
Erick Velez <erickvelez7@gmail.com>
2020-12-22 00:07:16 -0800

    Update to Twiiter v2 API

--- twitter.py
+++ twitter.py
@@ -1,49 +1,57 @@
 import json, requests, base64
+from requests_oauthlib import OAuth1Session
 import urllib.parse
 
 
 class Twitter:
-    BASE_URL = "https://api.twitter.com/1.1/"
-    USERS_SUFFIX = "users/"
-    OAUTH2_SUFFIX = "oauth2/"
-
     def __init__(self):
-        print("initializing Twitter API")
+        print("Initializing Twitter API connection...")
         with open('./keys.json') as key_file:
-            key_json = json.load(key_file)
-            self.secret = key_json['consumer_secret']
-            self.key = key_json['consumer_key']
-            self.authenticate()
+            key_json_file = json.load(key_file)
+        self.api_secret = key_json_file['api_secret']
+        self.api_key = key_json_file['api_key']
+        self.bearer_token = key_json_file['bearer_token']
+        self.authorized = False
+        
+    def create_authentication(self):
+        request_token_url = "https://api.twitter.com/oauth/request_token"
+        oauth = OAuth1Session(self.api_key, client_secret = self.api_secret)
+        auth_response = oauth.fetch_request_token(request_token_url)
+        resource_token = auth_response.get("oauth_token")
+        resource_secret = auth_response.get("oauth_token_secret")
+        base_authorization_url = "https://api.twitter.com/oauth/authorize"
+        authorization_url = oauth.authorization_url(base_authorization_url)
+        print("Please go here and authorize: %s" % authorization_url)
+        verifier = input("Paste the PIN here: ")
 
-    def show_user(self):
-        GET_SUFFIX = "show.json"
-        payload = {"screen_name": "Dominant_Colors"}
-        url = self.BASE_URL + self.USERS_SUFFIX + GET_SUFFIX;
-        headers = {
-            "Authorization": 'Bearer {}'.format(self.bearer_token),
-            "Accept-Ending": 'gzip'
-        }
-        user_object = requests.get(url, headers=headers, params=payload)
-        print(user_object.json())
+        oauth = OAuth1Session(
+            self.api_key,
+            client_secret=self.api_secret,
+            resource_owner_key=resource_token,
+            resource_owner_secret=resource_secret,
+            verifier=verifier
+        )
+        oauth_tokens = oauth.fetch_access_token("https://api.twitter.com/oauth/access_token")
+        oauth = OAuth1Session(
+            self.api_key,
+            client_secret = self.api_secret,
+            resource_owner_key=oauth_tokens.get("oauth_token"),
+            resource_owner_secret=oauth_tokens.get("oauth_token_secret")
+        )
+        self.oauth = oauth
 
-    def authenticate(self):
-        print("authenticating...")
+    def get_user(self):
+        if self.authorized is not True:
+            self.create_authentication()
+        fields = "created_at,description"
+        tweet_fields = "source,text,id,attachments,author_id"
+        params = {"usernames": "Dominant_Colors", "user.fields": fields, "tweet.fields" : tweet_fields} 
+        response = self.oauth.get("https://api.twitter.com/2/users/by", params=params)
+        if response.status_code != 200:
+            raise Exception(
+                "Request returned an error: {} {}".format(response.status_code, response.text)
+            )
+        
+        json_response = response.json()
 
-        consumer_key = urllib.parse.quote(self.key)
-        consumer_secret = urllib.parse.quote(self.secret)
-        bearer_token =  consumer_key + ':' + consumer_secret
-        base64_encoded_bearer_token = base64.b64encode(bearer_token.encode('utf-8'))
-
-        url = 'https://api.twitter.com/oauth2/token'
-        bearer_token_credentials = base64.urlsafe_b64encode('{}:{}'.format(consumer_key, consumer_secret).encode('ascii')).decode('ascii')
-        headers = {
-            'Authorization': 'Basic {}'.format(bearer_token_credentials),
-            'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8',
-        }
-        response = requests.post(url, headers=headers, data={'grant_type': 'client_credentials'})
-        response_json = response.json()
-        if response_json['token_type'] == 'bearer':
-            print("obtained bearer token")
-            self.bearer_token = response_json['access_token']
-        else:
-            raise RuntimeError('unexpected token type: {}'.format(response_json['token_type']))
+        print(json.dumps(json_response, indent=4, sort_keys=True))
